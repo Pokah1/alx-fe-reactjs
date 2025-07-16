eb20cd0fabcfe0ef1f115c66443d40b4dcdef719
Edikan Odokwo <edetedikan647@gmail.com>
2025-07-16 23:00:31 +0100

    Worked on UserProfile

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -6,7 +6,6 @@ function UserDetails() {
     <div>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
-      <p>Age: {userData.age}</p>
     </div>
   );
 }
--- alx-react-app-props/src/components/UserInfo.jsx
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -1,4 +1,4 @@
-import UserDetails from './UserProfile ';
+import UserDetails from './UserDetails';
 
 function UserInfo() {
   return <UserDetails />;
