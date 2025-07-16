ddfdc471a3c6d833784bf3cfb1de32e745f12e02
Edikan Odokwo <edetedikan647@gmail.com>
2025-07-16 22:50:21 +0100

    move userContext.js into component folder

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import ProfilePage from './components/ProfilePage';
-import UserContext from "../UserContext";
+import UserContext from "./components/UserContext";
 
 
 function App() {
