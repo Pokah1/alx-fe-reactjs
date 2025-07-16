f32cdfa2e65a9ad9a3012575937cbae9a1956930
Edikan Odokwo <edetedikan647@gmail.com>
2025-07-16 16:46:58 +0100

    created Counter component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Counter from "./components/Counter";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <MainContent />
+      <Counter />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
