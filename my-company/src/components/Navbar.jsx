f2cbf99087e37c8efe52181da863655091b4a662
Edikan Odokwo <edetedikan647@gmail.com>
2025-07-17 00:18:30 +0100

    fixed display styling for navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,13 +2,18 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav style={{ backgroundColor: '#56c452ff', padding: '15px', textAlign: 'center' }}>
+    <nav style={{
+      backgroundColor: '#2c3e50',
+      padding: '15px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
       <Link to="/" style={linkStyle}>Home</Link>
       <Link to="/about" style={linkStyle}>About</Link>
       <Link to="/services" style={linkStyle}>Services</Link>
       <Link to="/contact" style={linkStyle}>Contact</Link>
     </nav>
-           
   );
 };
 
@@ -16,7 +21,7 @@ const linkStyle = {
   color: '#fff',
   margin: '0 15px',
   textDecoration: 'none',
-  fontSize: '16px',
+  fontSize: '16px'
 };
 
 export default Navbar;
