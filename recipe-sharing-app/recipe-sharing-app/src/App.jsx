d79a185d4345af3d6b7e23c1986b71b02f2c13c1
Edikan Odokwo <edetedikan647@gmail.com>
2025-07-23 00:04:45 +0100

    feat: initial setup of recipe-sharing-app and install zustand

---
+++ recipe-sharing-app/recipe-sharing-app/src/App.jsx
@@ -0,0 +1,11 @@
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+function App() {
+  return <>
+  <AddRecipeForm />
+  <RecipeList />
+
+  </>;
+}
+
+export default App;
---
+++ recipe-sharing-app/recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -0,0 +1,36 @@
+import useRecipeStore from "../store/recipeStore";
+import { useState } from 'react';
+
+const AddRecipeForm = () => {
+    const addRecipe = useRecipeStore(state => state.addRecipe);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      addRecipe({ id: Date.now(), title, description });
+      setTitle('');
+      setDescription('');
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+        />
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Description"
+        />
+        <button type="submit">
+            Add Recipe
+        </button>
+      </form>
+    );
+  };
+
+  export default AddRecipeForm;
