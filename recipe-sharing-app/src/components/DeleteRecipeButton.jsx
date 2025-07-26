fd83665e5a45c2e62fa1ca0c63d96ac3236c1205
Edikan Odokwo <edetedikan647@gmail.com>
2025-07-26 23:19:51 +0100

    feat: modify DeleteRecipeButton to include useNavigate

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,22 +1,25 @@
 import useRecipeStore from "./recipeStore";
-
+import { useNavigate } from "react-router-dom";
 
 const DeleteRecipeButton = ({ recipeId, onDelete }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
-  
+  const navigate = useNavigate();
 
-  const handleDelete = () => {
+  const handleDelete = (event) => {
+    event.preventDefault();
     deleteRecipe(recipeId);
     alert('Recipe deleted successfully');
     if (onDelete) onDelete();
+    navigate('/');
   };
+  
 
   return (
     <button
       className="bg-red-500 text-white px-4 py-2 mt-2"
       onClick={handleDelete}
     >
-      Delete Recipe;
+      Delete Recipe
     </button>
   )
 };
