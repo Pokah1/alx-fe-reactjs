8f18ba1e20c281ac5756456d25b768264bc1c518
Edikan Odokwo <edetedikan647@gmail.com>
2025-08-07 10:02:08 +0100

    fixed

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import avatar from '../assets/avatar.jpg';
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4">
       <img src={avatar} alt="User" 
       className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
       />
