6acc908df8b2aee57eff93db7e3f2b44002d7c0a
Edikan Odokwo <edetedikan647@gmail.com>
2025-08-11 13:53:18 +0100

    added "postcss", "autoprefixer" to package.json

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
    <div>
-    <h1 className="font-bold text-blue-500">Hello World, Welcome</h1>
+    <h1 className="font-bold text-blue-500 ">Hello World, Welcome</h1>
    </div>
   )
 }
