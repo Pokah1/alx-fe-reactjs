b9f8a06d7df924f6b7c79213b3ea5c50f25718d7
Edikan Odokwo <edetedikan647@gmail.com>
2025-08-22 22:27:56 +0100

    to fix reuired issue for checker

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -4,18 +4,13 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object({
-  username: Yup.string()
-  .required("Username is required")
-  .min(3, "Username must be at least 3 characters"),
-
-email: Yup.string()
-  .required("Email is required")
-  .email("Invalid email format"),
-
-password: Yup.string()
-  .required("Password is required")
-  .min(6, "Password must be at least 6 characters"),
-
+  username: Yup.string().required("Username is required"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
 });
 
 const FormikForm = () => {
