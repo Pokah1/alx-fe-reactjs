649c2501a4ed6f66b76c2e617244e3ea5a3944b6
Edikan Odokwo <edetedikan647@gmail.com>
2025-08-22 22:18:00 +0100

    Fixing Yup password required issue

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -5,14 +5,17 @@ import * as Yup from "yup";
 
 const validationSchema = Yup.object({
   username: Yup.string()
-    .min(3, "Username must be at least 3 characters")
-    .required("Username is required"),
-  email: Yup.string()
-    .email("Invalid email format")
-    .required("Email is required"),
-  password: Yup.string()
-    .min(6, "Password must be at least 6 characters")
-    .required("Password is required"),
+  .required("Username is required")
+  .min(3, "Username must be at least 3 characters"),
+
+email: Yup.string()
+  .required("Email is required")
+  .email("Invalid email format"),
+
+password: Yup.string()
+  .required("Password is required")
+  .min(6, "Password must be at least 6 characters"),
+
 });
 
 const FormikForm = () => {
