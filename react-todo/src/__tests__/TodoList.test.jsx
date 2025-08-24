b78ede27b09723585dccff52fe9313898ca69736
Edikan Odokwo <edetedikan647@gmail.com>
2025-08-24 01:16:25 +0100

    feat: add TodoList with CRUD and tests

---
+++ react-todo/src/__tests__/TodoList.test.jsx
@@ -0,0 +1,35 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+test("renders initial todos", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build Todo App")).toBeInTheDocument();
+   expect(screen.getByText("Complete my Portfolio")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  const input = screen.getByPlaceholderText("Add a todo");
+  const button = screen.getByText("Add");
+
+  fireEvent.change(input, { target: { value: "New Todo" } });
+  fireEvent.click(button);
+
+  expect(screen.getByText("New Todo")).toBeInTheDocument();
+});
+
+test("toggles a todo", () => {
+  render(<TodoList />);
+  const todo = screen.getByText("Learn React");
+  fireEvent.click(todo);
+  expect(todo).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+  const deleteBtn = screen.getAllByText("Delete")[0];
+  fireEvent.click(deleteBtn);
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+});
